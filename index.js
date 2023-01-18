c59c87e8bfe0d22741aa83e2c5e7fcf3cc359820
hlampton <112996304+hlampton@users.noreply.github.com>
2023-01-17 19:00:08 -0600

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
+// import libraries
+const inquirer = require("inquirer");
+const fs = require("fs");
+const generateTeam = require("./src/page-template.js");
 
+// import staff class
+const Engineer = require("./lib/Engineer");
+const Intern = require("./lib/Intern");
+const Manager = require("./lib/Manager");
